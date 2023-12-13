7e881d14a30e20d4f27c1b55ca4b800c3f4e46fa
Tatiana Mikhailova <imagetata@gmail.com>
2023-12-13 14:56:18 +0100

    no connection to any db

---
+++ src/main/java/salt/lab/restwithvalidation/RestWithValidationApplication.java
@@ -0,0 +1,13 @@
+package salt.lab.restwithvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/salt/lab/restwithvalidation/RestWithValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package salt.lab.restwithvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
